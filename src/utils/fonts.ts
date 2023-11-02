ac6638125d6401b9134f929c6c34053bcfa13fb1
zakariamagdyz <zakariamagdy@ymail.com>
2023-11-02 21:26:51 +0200

    feat: implement design system for Medex app (colors, typography, responsiveness, fonts)

---
+++ src/utils/fonts.ts
@@ -0,0 +1,8 @@
+import { Roboto } from "next/font/google";
+
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  variable: "--font-roboto",
+  display: "swap",
+});
