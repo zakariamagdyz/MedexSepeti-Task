33b7152fa8bd21b21e5cc184897cf7ff6047ffc0
zakariamagdyz <zakariamagdy@ymail.com>
2023-11-02 23:55:34 +0200

    feat: add utility classess to the design system

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+import clsx from "clsx";
+
+export default function Home() {
+  return <main className={clsx("m-10")}>Medex</main>;
+}
